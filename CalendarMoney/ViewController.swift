0c21665a9bbb514ae1bec5c777ec54b57097f9cc
l08084 <l08084@shibaura-it.ac.jp>
2016-05-14 18:26:27 +0900

    新規プロジェクト作成

---
+++ CalendarMoney/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalendarMoney
+//
+//  Created by 中安拓也 on 2016/05/14.
+//  Copyright © 2016年 l08084. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
