bbb113ef2c78b9cb233f7ecdcda7bd194d50451e
l08084 <l08084@shibaura-it.ac.jp>
2016-05-17 23:56:44 +0900

    add moneyField,memoField

--- CalendarMoney/MoneyInputVIewController.swift
+++ CalendarMoney/MoneyInputVIewController.swift
@@ -2,8 +2,15 @@ import UIKit
 
 class MoneyInputViewController: UIViewController {
     
+    @IBOutlet weak var moneyField: UITextField!
+    @IBOutlet weak var memoTextVIew: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 影の濃さを設定する
+        memoTextVIew.layer.shadowOpacity = 0.5
     }
     
     override func didReceiveMemoryWarning() {
